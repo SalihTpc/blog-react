902d2a9abe7df1c184de6db3d16c95cae3dc5a51
SalihTpc <salihtopcu@hotmail.com.tr>
2022-04-01 20:56:33 +0300

    Approuter added.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import MyCards from "../components/MyCards";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <MyCards />
+    </div>
+  );
+};
+
+export default Home;
