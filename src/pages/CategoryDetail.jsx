85f249d064f935f5488a723533c5ee50159c41fb
SalihTpc <salihtopcu@hotmail.com.tr>
2022-04-07 20:21:41 +0300

    CategoryDetail is added.

---
+++ src/pages/CategoryDetail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CategoryCard from "../components/CategoryCard";
+import Navbar from "../components/Navbar";
+
+const CategoryDetail = () => {
+  return (
+    <div>
+      <Navbar />
+      <CategoryCard />
+    </div>
+  );
+};
+
+export default CategoryDetail;
