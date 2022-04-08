93e34357a6336f6d48fad15e6e3a0320f5477d6f
SalihTpc <salihtopcu@hotmail.com.tr>
2022-04-09 01:28:47 +0300

    mapping added.

--- src/pages/CategoryDetail.jsx
+++ src/pages/CategoryDetail.jsx
@@ -2,13 +2,34 @@ import React from "react";
 import CategoryCard from "../components/CategoryCard";
 import Navbar from "../components/Navbar";
 import { useParams } from "react-router-dom";
+import { BlogContext } from "../store/BlogContext";
+import axios from "axios";
+import Loader from "../components/Loader";
 
 const CategoryDetail = () => {
   const { str } = useParams();
+  const [selectedCategoryPosts, setSelectedCategoryPosts] = React.useState([]);
+  const { load, setLoad } = React.useContext(BlogContext);
+  const getSelectedCategories = async () => {
+    setLoad(true);
+    await axios
+      .get(`https://blogsato-drf.herokuapp.com/api/category/${str}/`)
+      .then((response) => setSelectedCategoryPosts(response.data));
+    setLoad(false);
+  };
+  React.useEffect(() => {
+    getSelectedCategories();
+  }, [str]);
   return (
-    <div>
+    <div className="categoryposts">
       <Navbar />
-      <CategoryCard categoryName={str} />
+      {load ? (
+        <Loader />
+      ) : (
+        selectedCategoryPosts.map((post) => (
+          <CategoryCard key={post.id} post={post} />
+        ))
+      )}
     </div>
   );
 };
