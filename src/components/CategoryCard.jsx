f704edcce1dd85b0f8351a21ddef24e7209eee29
SalihTpc <salihtopcu@hotmail.com.tr>
2022-04-09 01:28:30 +0300

    mappin carried to up element

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -15,97 +15,76 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import { CardActionArea } from "@mui/material";
 import CommentIcon from "@mui/icons-material/Comment";
 import Badge from "@mui/material/Badge";
-import axios from "axios";
 import "./categorycard.css";
 
-const CategoryCard = ({ categoryName }) => {
-  const [selectedCategoryPosts, setSelectedCategoryPosts] = React.useState([]);
-  const getSelectedCategories = async () => {
-    await axios
-      .get(`https://blogsato-drf.herokuapp.com/api/category/${categoryName}/`)
-      .then((response) => setSelectedCategoryPosts(response.data));
-  };
-  // React.useEffect(() => {
-  //   getSelectedCategories();
-  // }, []);
-
-  React.useEffect(() => {
-    getSelectedCategories();
-  }, [categoryName]);
-
-  // console.log(selectedCategoryPosts);
-
+const CategoryCard = ({ post }) => {
   return (
-    <div className="categoryposts">
-      {selectedCategoryPosts.map((post) => (
-        <Card key={post.id} sx={{ maxWidth: 345, m: 2 }}>
-          <CardActionArea>
-            <CardHeader
-              avatar={
-                <Avatar
-                  sx={{ bgcolor: blue[200] }}
-                  aria-label="recipe"
-                  title={post.user}
-                  alt={post.user.charAt(0).toUpperCase()}
-                  src="/static/images/avatar/2.jpg"
-                />
-              }
-              titleTypographyProps={{ variant: "h5", color: "black" }}
-              title={post.title}
-              subheader={post.since_creation}
+    <Card key={post.id} sx={{ maxWidth: 345, m: 2 }}>
+      <CardActionArea>
+        <CardHeader
+          avatar={
+            <Avatar
+              sx={{ bgcolor: blue[200] }}
+              aria-label="recipe"
+              title={post.user}
+              alt={post.user.charAt(0).toUpperCase()}
+              src="/static/images/avatar/2.jpg"
             />
-            <Divider />
-            <Box sx={{ display: "flex" }}>
-              {post.category.map((cate) => (
-                <Typography
-                  key={cate.id}
-                  sx={{ flexGrow: 1, mt: 0.5, ml: 2 }}
-                  color="text.secondary"
-                  display="block"
-                  variant="caption"
-                >
-                  {cate.name}
-                </Typography>
-              ))}
-            </Box>
+          }
+          titleTypographyProps={{ variant: "h5", color: "black" }}
+          title={post.title}
+          subheader={post.since_creation}
+        />
+        <Divider />
+        <Box sx={{ display: "flex" }}>
+          {post.category.map((cate) => (
+            <Typography
+              key={cate.id}
+              sx={{ flexGrow: 1, mt: 0.5, ml: 2 }}
+              color="text.secondary"
+              display="block"
+              variant="caption"
+            >
+              {cate.name}
+            </Typography>
+          ))}
+        </Box>
 
-            <CardMedia
-              component="img"
-              height="550"
-              image={post.image}
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                {post.body.length > 300
-                  ? post.body.slice(0, 300) + " ..."
-                  : post.body}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions
-            sx={{ display: "flex", justifyContent: "space-around" }}
-            disableSpacing
-          >
-            <IconButton aria-label="like">
-              <Badge badgeContent={post.likes_count} color="error">
-                <FavoriteIcon />
-              </Badge>
-            </IconButton>
-            <IconButton aria-label="view">
-              <Badge badgeContent={post.postviews_count} color="info">
-                <VisibilityIcon />
-              </Badge>
-            </IconButton>
-            <IconButton aria-label="comment">
-              <Badge badgeContent={post.comments_count} color="success">
-                <CommentIcon />
-              </Badge>
-            </IconButton>
-          </CardActions>
-        </Card>
-      ))}
-    </div>
+        <CardMedia
+          component="img"
+          height="550"
+          image={post.image}
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {post.body.length > 300
+              ? post.body.slice(0, 300) + " ..."
+              : post.body}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions
+        sx={{ display: "flex", justifyContent: "space-around" }}
+        disableSpacing
+      >
+        <IconButton aria-label="like">
+          <Badge badgeContent={post.likes_count} color="error">
+            <FavoriteIcon />
+          </Badge>
+        </IconButton>
+        <IconButton aria-label="view">
+          <Badge badgeContent={post.postviews_count} color="info">
+            <VisibilityIcon />
+          </Badge>
+        </IconButton>
+        <IconButton aria-label="comment">
+          <Badge badgeContent={post.comments_count} color="success">
+            <CommentIcon />
+          </Badge>
+        </IconButton>
+      </CardActions>
+    </Card>
   );
 };
 
