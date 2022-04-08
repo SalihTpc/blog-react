dd7712d634803bbcaf3aa9fabe5715e81b3c7f71
SalihTpc <salihtopcu@hotmail.com.tr>
2022-04-08 22:35:15 +0300

    Loader added

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+const Loader = () => {
+  return;
+  <Triangle height="100" width="100" color="grey" ariaLabel="loading" />;
+};
+
+export default Loader;
