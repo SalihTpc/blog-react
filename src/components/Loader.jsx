b14d3d0b98a4f870fd185fd27fc65d2916987048
SalihTpc <salihtopcu@hotmail.com.tr>
2022-04-08 22:40:23 +0300

    Triangle imported

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Triangle } from "react-loader-spinner";
 
 const Loader = () => {
   return;
