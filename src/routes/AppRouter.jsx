844bf946cb92828c2e74a2df7d299a4fa01ec6cd
SalihTpc <salihtopcu@hotmail.com.tr>
2022-04-12 00:20:48 +0300

    PostDetail added to Router

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import CategoryDetail from "../pages/CategoryDetail";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import PostDetail from "../pages/PostDetail";
 import Register from "../pages/Register";
+import { BlogContext } from "../store/BlogContext";
 
 const AppRouter = () => {
+  const { isAuth } = useContext(BlogContext);
+  function RequireAuth({ children, redirectTo }) {
+    if (!isAuth) {
+      return <Navigate to={redirectTo} />;
+    }
+    return children;
+  }
   return (
     <BrowserRouter>
       <Routes>
@@ -13,6 +22,14 @@ const AppRouter = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/category/:str" element={<CategoryDetail />} />
+        <Route
+          path="/post-detail/:id"
+          element={
+            <RequireAuth redirectTo={"/login"}>
+              <PostDetail />
+            </RequireAuth>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
