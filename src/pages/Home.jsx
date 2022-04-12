1227b182008b033add989f69aa89b75301df8ce2
SalihTpc <salihtopcu@hotmail.com.tr>
2022-04-13 02:38:49 +0300

    nothing changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,12 @@
 import React, { useContext } from "react";
-import Navbar from "../components/Navbar";
 import MyCards from "../components/MyCards";
 import Loader from "../components/Loader";
 import { BlogContext } from "../store/BlogContext";
 
 const Home = () => {
-  const { load, user } = useContext(BlogContext);
-  return (
-    <div className="home">
-      <h1>{user.username}</h1>
-      {/* <MyCards /> */}
-      {load ? <Loader /> : <MyCards />}
-    </div>
-  );
+  const { load } = useContext(BlogContext);
+
+  return <div className="home">{load ? <Loader /> : <MyCards />}</div>;
 };
 
 export default Home;
