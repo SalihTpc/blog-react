43d6faa32f516f7d8e022be9f458d10fa1fb472d
SalihTpc <salihtopcu@hotmail.com.tr>
2022-04-13 02:34:03 +0300

    ToastContainer added.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import AppRouter from "./routes/AppRouter";
 import { BlogProvider } from "./store/BlogContext";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <BlogProvider>
       <AppRouter />
+      <ToastContainer />
     </BlogProvider>
   );
 }
