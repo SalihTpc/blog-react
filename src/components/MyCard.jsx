1a58abf0805d9960fc304bdf3dd7789032f5dd18
SalihTpc <salihtopcu@hotmail.com.tr>
2022-04-17 04:19:46 +0300

    at home page user can add your comment if auth

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -12,17 +12,88 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import Divider from "@mui/material/Divider";
 import Box from "@mui/material/Box";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import { CardActionArea } from "@mui/material";
+import {
+  CardActionArea,
+  InputAdornment,
+  InputLabel,
+  Modal,
+  OutlinedInput,
+} from "@mui/material";
 import CommentIcon from "@mui/icons-material/Comment";
 import Badge from "@mui/material/Badge";
 import { useNavigate } from "react-router-dom";
 import { errorNote, successNote } from "../helper/toastNotify";
 import axios from "axios";
 import { BlogContext } from "../store/BlogContext";
+import SendIcon from "@mui/icons-material/Send";
+
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  maxWidth: "100%",
+  boxShadow: 24,
+  borderRadius: 10,
+  opacity: 0.9,
+  bgcolor: "background.paper",
+  p: 1,
+};
+const myStyle1 = {
+  position: "relative",
+  width: "95%",
+};
 
 const MyCard = ({ anime }) => {
   let navigate = useNavigate();
   const { myChanges, setMyChanges } = React.useContext(BlogContext);
+  const myId = anime.id;
+  const [commentValue, setCommentValue] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    sessionStorage.getItem("key") ? setOpen(true) : navigate("/login");
+  };
+  const handleClose = () => setOpen(false);
+
+  const inputTextHandler = (e) => {
+    setCommentValue(e.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const value = {
+      post: myId,
+      content: data.get("comment"),
+    };
+    data.get("comment").length === 0
+      ? errorNote("Comment can not be blank!!")
+      : CommentPost(myId, value);
+  };
+
+  const CommentPost = async (id, value) => {
+    try {
+      await axios
+        .post(
+          `https://blogsato-drf.herokuapp.com/api/list/${id}/comment/`,
+          value,
+          {
+            headers: {
+              Authorization: `Token ${sessionStorage.getItem("key")}`,
+            },
+          }
+        )
+        .then(function (response) {
+          // console.log(response.data.message);
+          successNote(response.data.message);
+        });
+      setMyChanges(!myChanges);
+      setCommentValue("");
+    } catch (error) {
+      // console.log(error.response.data.content[0]);
+      errorNote(error.response.data.content[0]);
+    }
+  };
 
   const viewPost = async (id) => {
     const value = { post: id };
@@ -41,11 +112,14 @@ const MyCard = ({ anime }) => {
           // console.log(response.data.user);
           successNote(`${response.data.user} viewed the Post`);
         });
-      // navigate("/");
       setMyChanges(!myChanges);
     } catch (error) {
-      // console.log(error.response.data.non_field_errors[0]);
-      errorNote(error.response.data.non_field_errors[0]);
+      // console.log(
+      //   error.response.data.detail || error.response.data.non_field_errors[0]
+      // );
+      errorNote(
+        error.response.data.detail || error.response.data.non_field_errors[0]
+      );
     }
   };
   const likePost = async (id) => {
@@ -65,11 +139,13 @@ const MyCard = ({ anime }) => {
           // console.log(response.data.user);
           successNote(`${response.data.user} liked the Post`);
         });
-      // navigate("/");
       setMyChanges(!myChanges);
     } catch (error) {
-      // console.log(error.response.data.non_field_errors[0]);
-      errorNote(error.response.data.non_field_errors[0]);
+      console.log(error.response.data);
+      // errorNote(error.response.data.non_field_errors[0]);
+      errorNote(
+        error.response.data.detail || error.response.data.non_field_errors[0]
+      );
     }
   };
 
@@ -133,11 +209,57 @@ const MyCard = ({ anime }) => {
             <VisibilityIcon />
           </Badge>
         </IconButton>
-        <IconButton aria-label="comment">
+        <IconButton onClick={handleOpen} aria-label="comment">
           <Badge badgeContent={anime.comments_count} color="success">
             <CommentIcon />
           </Badge>
         </IconButton>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={modalStyle}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{
+                width: 900,
+                maxWidth: "100%",
+                mb: 2,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <div style={myStyle1}>
+                <InputLabel>Comment</InputLabel>
+                <OutlinedInput
+                  value={commentValue}
+                  onChange={inputTextHandler}
+                  fullWidth
+                  htmlFor="outlined-adornment-comment"
+                  required
+                  id="comment"
+                  name="comment"
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        disabled={commentValue.length === 0}
+                        type="submit"
+                        aria-label="comment"
+                      >
+                        <SendIcon size="large" />
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </div>
+            </Box>
+          </Box>
+        </Modal>
       </CardActions>
     </Card>
   );
